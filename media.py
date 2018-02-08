72c51334aa18109d3d6b9a63b1f2a0de2fe45b63
rphadol <rajshree.phadol28@gmail.com>
2018-02-08 14:18:55 -0500

    Add files via upload

--- media.py
+++ media.py
@@ -1,18 +1,22 @@
 import webbrowser
 
-#created class Movie
+# created class Movie
+
+
 class Movie():
     """This class provides a way to store movie related info."""
-    #VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    #define init funtcion to intialize tilte of movie, storyline, poster image, youtube trailer
+    # VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    # defined init funtcion to intialize /
+    # tilte ,storyline,poster image,youtube trailer
+
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-        
-    #new method to show trailer  
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
-   
+# new method to show trailer
+
+
+def show_trailer(self):
+    webbrowser.open(self.trailer_youtube_url)
